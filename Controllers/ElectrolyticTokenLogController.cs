827dbc23d757c5f8f6cf96f9d073f997e2e7c37a
Tuandinhtranz <tuan.dinhtran.set18@eiu.edu.vn>
2023-05-15 10:05:48 +0700

    Create ElectrolyticTokenLogController.cs

---
+++ Controllers/ElectrolyticTokenLogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BecaworkService.Controllers
+{
+    public class ElectrolyticTokenLogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
